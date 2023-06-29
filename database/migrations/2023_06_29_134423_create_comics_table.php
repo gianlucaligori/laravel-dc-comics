543bbca15f12da1a6fabe11279535bb37a57861e
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-06-29 15:55:24 +0200

    first commit

---
+++ database/migrations/2023_06_29_134423_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->text('thumb');
+            $table->string('price', 10);
+            $table->string('series', 20);
+            $table->data('sale_data');
+            $table->string('type', 20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
