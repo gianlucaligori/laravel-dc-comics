e0c0b7f305df0042e365e60af3c7cdb018a28f96
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-06-30 10:50:33 +0200

    file controller in the wrong folder - fixed

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Guest\PageController;
 use App\Http\Controllers\Guest\ComicController;
 
-Route::get('/', function () {
-    return view('index');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
 Route::get('/', [PageController::class, 'comics']);
 
 Route::resource('comics', ComicController::class);
