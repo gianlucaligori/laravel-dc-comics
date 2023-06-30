5e92f2217e3cc60abcda1b3a0637f75983a3334d
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-06-30 11:17:19 +0200

    index, show, create fixes

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,4 +1,9 @@
 @extends('comics')
 
 @section('contents')
+    <div> {{ $comic->title }}</div>
+    <div> {{ $comic->price }}</div>
+    <div> {{ $comic->series }}</div>
+    <div> {{ $comic->sale_date }}</div>
+    <div>{{ $comic->type }}</div>
 @endsection
